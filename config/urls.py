a0fb4e66406891a62bb086945b2f7c277d2cfec9
Alex Baikov <baikov52@gmail.com>
2021-02-17 15:48:46 +0300

    countries app

--- config/urls.py
+++ config/urls.py
@@ -16,6 +16,7 @@ urlpatterns = [
     path("users/", include("flags.users.urls", namespace="users")),
     path("accounts/", include("allauth.urls")),
     # Your stuff: custom urls includes go here
+    path("countries/", include("countries.urls", namespace="countries")),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 
