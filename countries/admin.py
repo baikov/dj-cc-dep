53bfcce1829cc2f4a2d90ded940e39ae4841e593
Alex Baikov <baikov52@gmail.com>
2021-02-19 20:01:24 +0300

    fix admin field output

--- countries/admin.py
+++ countries/admin.py
@@ -5,17 +5,41 @@ from django.db import models
 
 class CountryAdmin(admin.ModelAdmin):
     prepopulated_fields = {'slug': ('name',)}
-    list_display = ('name', 'slug')
+    list_display = ('name', 'slug', 'is_published')
     list_filter = ['name']
     search_fields = ['name']
-    # fieldsets = [
-    #     (None, {'fields': ['is_published', 'title', 'is_highlighted', 'is_toc', 'toc_depth', 'short_text', 'image', 'body']}),
-    #     ('SEO', {'fields': ['slug', 'seo_title', 'seo_description']}),
-    # ]
+    readonly_fields = ['updated_date', 'created_date']
+    fieldsets = [
+        (None, {
+                'fields': [
+                    'name', 'slug', 'conventional_long_name',
+                    'local_long_name', 'local_short_name', 'capital_name',
+                    'continent', 'anthem', 'motto', 'official_language',
+                    'national_language', 'internet_tld', 'currency_name',
+                    'currency_code', 'currency_simbol', 'iso_code_a2',
+                    'iso_code_a3', 'iso_code_num'
+                    ]
+                }),
+        ('SEO', {
+                'fields': [
+                    'meta_title', 'meta_description', 'meta_keywords', 
+                    'meta_h1', 'is_published', 'is_index', 'is_follow',
+                    'created_date', 'updated_date',
+                    ]
+                }),
+    ]
+    def get_form(self, request, obj=None, **kwargs):
+        form = super(CountryAdmin, self).get_form(request, obj, **kwargs)
+        form.base_fields['meta_description'].widget.attrs['rows'] = 2
+        form.base_fields['meta_description'].widget.attrs['cols'] = 10 # doesn't work...
+        return form
+    formfield_overrides = {
+        models.CharField: {'widget': TextInput(attrs={'size':'100'})},
+    }
 
 class FlagAdmin(admin.ModelAdmin):
     prepopulated_fields = {'slug': ('title',)}
-    list_display = ('title', 'slug', 'colors')
+    list_display = ('title', 'slug', 'is_published')
     search_fields = ['title']
     readonly_fields = ['updated_date', 'created_date']
     fieldsets = [
