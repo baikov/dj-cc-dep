67eb26b2726064c2126b54581b2ab2a3334293d1
Alex Baikov <baikov52@gmail.com>
2021-02-19 19:05:53 +0300

    add SEO abstract class

--- countries/models.py
+++ countries/models.py
@@ -1,15 +1,33 @@
 from django.db import models
+from django.contrib.postgres.fields import ArrayField
+
+class Seo (models.Model):
+    class Meta:
+        abstract = True
+        
+    meta_title = models.CharField(verbose_name='SEO Title', max_length=250, blank=True)
+    meta_description = models.TextField(
+        max_length=400, verbose_name='SEO Description', blank=True)
+    meta_keywords = models.CharField(verbose_name='SEO keywords', max_length=250, blank=True)
+    meta_h1 = models.CharField(verbose_name='SEO H1', max_length=250, blank=True)
+    is_published = models.BooleanField(verbose_name='Опубликовано', default=False, null=False)
+    # is_highlighted = models.BooleanField(verbose_name='Featured', default=False)
+    is_index = models.BooleanField(verbose_name='index', default=True, null=False)
+    is_follow = models.BooleanField(verbose_name='follow', default=True, null=False)
+    # shemaorg_markup
+    created_date = models.DateField(auto_now_add=True)
+    updated_date = models.DateField(auto_now=True)
 
 class Country (models.Model):
 
     class Continents(models.TextChoices):  	  	  	  	  	  
-        AFRICA = 'AF', _('Африка')
-        ASIA = 'AS', _('Евразия')
-        EUROPE = 'EU', _('Евразия')
-        NORTH_AMERICA = 'NA', _('Северная Америка')
-        SOUTH_AMERICA = 'SA', _('Южная Америка')
-        AUSTRALIA = 'AU', _('Австралия')
-        EMPTY = 'NO', _('')
+        AFRICA = 'AF', 'Африка'
+        ASIA = 'AS', 'Евразия (Азия)'
+        EUROPE = 'EU', 'Евразия (Европа)'
+        NORTH_AMERICA = 'NA', 'Северная Америка'
+        SOUTH_AMERICA = 'SA', 'Южная Америка'
+        AUSTRALIA = 'AU', 'Австралия'
+        EMPTY = 'NO', ''
 
     name = models.CharField(verbose_name='Название', max_length=250)
     conventional_long_name = models.CharField(verbose_name='Официальное название', max_length=250)
@@ -19,6 +37,7 @@ class Country (models.Model):
     continent = models.CharField(verbose_name='Континент', 
                                 choices=Continents.choices,
                                 default=Continents.EMPTY,
+                                max_length=50,
                                 )
     anthem = models.URLField(verbose_name='Гимн', max_length=250, blank=True)
     motto = models.CharField(verbose_name='Девиз', max_length=250, blank=True)
@@ -36,3 +55,20 @@ class Country (models.Model):
 
     def __str__(self):
         return self.name
+
+class Flag(Seo, models.Model):
+    country = models.ForeignKey(to=Country, on_delete=models.CASCADE, related_name='flags')
+    title = models.CharField(verbose_name='Заголовок', max_length=250)
+    slug = models.SlugField(max_length=100, unique=True,)
+    name = models.CharField(verbose_name='Название', max_length=250, blank=True)
+    date = models.DateField(verbose_name='Дата утверждения', blank=True,)
+    # use_for = models.CharField(verbose_name='Название', max_length=250, blank=True)
+    proportion = models.CharField(verbose_name='Пропорции', max_length=10, blank=True)
+    colors = ArrayField(models.CharField(max_length=200), blank=True, null=True)
+    figure = models.CharField(verbose_name='Фигура на флаге', max_length=250, blank=True)
+    emoji = models.CharField(verbose_name='Эмоджи', max_length=20, blank=True)
+    short_description = models.TextField(
+        max_length=550, verbose_name='Краткое описание', blank=True)
+
+    def __str__(self):
+        return self.title
