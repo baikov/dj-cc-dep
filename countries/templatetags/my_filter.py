cf6ca0979f6124313162315a802503b89d97846f
Alex Baikov <baikov52@gmail.com>
2021-03-03 12:01:34 +0300

    add tag for output meters in km

--- countries/templatetags/my_filter.py
+++ countries/templatetags/my_filter.py
@@ -7,3 +7,9 @@ register = template.Library()
 def get_name(object, field):
     verbose_name = object._meta.get_field(field).verbose_name
     return verbose_name
+
+
+@register.filter()
+def in_km(field):
+    width = f'{field/1000} км'
+    return width
